982d27ceba0739473a4a3a907abe1f5c5db7c24b
Jean-Ludovic <ludotoro02@gmail.comm>
2023-11-16 07:42:54 -0500

    2

---
+++ config/connexion.php
@@ -0,0 +1,17 @@
+<?php
+
+//  les informations de connexion de votre base de données
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "ecom1_tp2";
+
+// Création de la connexion
+$conn = mysqli_connect($host, $username, $password, $database);
+
+// Vérification de la connexion
+if (!$conn) {
+    die("Erreur de connexion à la base de données : " . mysqli_connect_error());
+}
+
+?>
