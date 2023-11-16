a81fd0a2a5a00342744b94c9c774a3e594eee7ed
Jean-Ludovic <ludotoro02@gmail.comm>
2023-11-16 11:58:20 -0500

    3

--- index.php
+++ index.php
@@ -1,16 +1,32 @@
-<!DOCTYPE html>
-    <html lang="en">
-    <head>
-     
-    </head>
-    <body>
-        <h1>Bienvenue sur notre page</h1>
-        <br><br>
-        <a href="pages/signup.php">Signup</a>
-        <br><br>
-        <a href="pages/login.php">Login</a>
-    </body>
-    </html>
-    <?php
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['num_addresses'])) {
+        $_SESSION['num_addresses'] = (int)$_POST['num_addresses'];
+        header("Location: index.php");
+        exit();
+    } else {
+        saveFormDataToSession();
+        header("Location: confirmation.php");
+        exit();
+    }
+}
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Adresse Form</title>
+</head>
+<body>
+    <?php
+    if (isset($_SESSION['num_addresses'])) {
+        displayAddressForm($_SESSION['num_addresses']);
+    } else {
+        displayNumAddressesForm();
+    }
+    ?>
+</body>
+</html>
