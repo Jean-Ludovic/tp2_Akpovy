6498ea7fac8e48af8b84a049c4f6d1a8f8ad57c4
Jean-Ludovic <ludotoro02@gmail.comm>
2023-11-16 07:34:22 -0500

    1

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+     
+    </head>
+    <body>
+        <h1>Bienvenue sur notre page</h1>
+        <br><br>
+        <a href="pages/signup.php">Signup</a>
+        <br><br>
+        <a href="pages/login.php">Login</a>
+    </body>
+    </html>
+    <?php
+
+?>
